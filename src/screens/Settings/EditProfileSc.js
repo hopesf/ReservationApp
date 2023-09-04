35cf1562c18f21cb2a7b276822a342bab23f000c
Selim Geçin <gecin18@gmail.com>
2023-09-04 08:01:53 +0300

    🎸last

--- src/screens/Settings/EditProfileSc.js
+++ src/screens/Settings/EditProfileSc.js
@@ -1,8 +1,21 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { MaterialIcons } from "@expo/vector-icons";
+import { COLORS, FONTS } from "../../constants";
+
 export default function EditProfileSc() {
   return (
-    <View>
-      <Text>EditProfileSc</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
+      <View style={{ marginHorizontal: 12, flexDirection: "row", justifyContent: "center", marginVertical: 12 }}>
+        <TouchableOpacity onPress={() => navigation.goBack()} style={{ position: "absolute", left: 0 }}>
+          <MaterialIcons name="keyboard-arrow-left" size={24} color={COLORS.black} />
+        </TouchableOpacity>
+
+        <Text style={{ ...FONTS.h3 }}>Profili Düzenle</Text>
+      </View>
+      <View style={{ marginHorizontal: 12 }}>
+        <Text>Profile</Text>
+      </View>
+    </SafeAreaView>
   );
 }
